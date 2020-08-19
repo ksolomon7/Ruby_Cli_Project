58d7de431243e9b5b917256bc28cdc9cae7adadc
ksolomon7 <kalkidan.solomon2@gmail.com>
2020-08-19 11:45:44 -0400

    add create and view functionality for event_planner

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,53 +6,14 @@ class Event < ActiveRecord::Base
   attr_accessor :yourevent
 
   def self.view_events(role)
-    puts "You have #{role.events.count} events!"
-    parties= Event.where(client_id: role.id)
-    options=parties.map do |party|
-      party.event_name
-    end
-
-    prompt=TTY::Prompt.new
-    move= prompt.select("\n", options)
-
-    options.select do |event|
-      if event == move
-        found_event=Event.find_by(event_name: move)
-        
-        puts "***********************************************************************".colorize(:green) 
-        puts "\n"
-        puts "EVENT NAME: #{found_event.event_name}".colorize(:yellow)
-        puts "DATE: #{found_event.date}".colorize(:yellow)
-        puts "LOCATION: #{found_event.location}".colorize(:yellow)
-        puts "DURATION: #{found_event.duration}".colorize(:yellow)
-        puts "\n"
-          if found_event.event_planner != nil
-        puts "EVENTPLANNER: #{found_event.event_planner.name}".colorize(:red)
-          elsif found_event.event_planner == nil
-        puts "EVENTPLANNER: None".colorize(:red)
-          end
-          if found_event.client != nil
-        puts "CLIENT: #{found_event.client.name}".colorize(:red)
-          elsif found_event.client == nil
-        puts "CLIENT ACCOUNT NUMBER: #{found_event.client.id}".colorize(:red)
-          end
-        puts "\n"
-        puts "***********************************************************************".colorize(:green) 
-      end
-    end
-    puts "\n"
-    puts "....going back to the main page!!".colorize(:yellow)
-    sleep 4
-    YourEvent.home_page(role)
+    Event.is_a_client_or_planner_viewing_event?(role)
   end 
 
   def self.create_events(role)
     Event.is_a_client_or_an_event_planner?(role)
-   
-      # Event.create(result)
-      # binding.pry
-      # Event.view_events(role)
-    end
+  end
+
+    
   ################################################HELPER METHOD#####################################################################
 
   def self.is_a_client_or_an_event_planner?(role)
@@ -60,6 +21,14 @@ class Event < ActiveRecord::Base
       Client.create_event_for_client(role)
     elsif EventPlanner.find_by(email: role.email)
       EventPlanner.create_event_for_event_planner(role)
+    end
+  end
+
+  def self.is_a_client_or_planner_viewing_event?(role)
+    if Client.find_by(email: role.email)
+      Client.view_event_for_client(role)
+    elsif EventPlanner.find_by(email: role.email)
+      EventPlanner.view_event_for_event_planner(role)
     end
   end
 end
--- app/models/eventplanner.rb
+++ app/models/eventplanner.rb
@@ -4,6 +4,47 @@ class EventPlanner < ActiveRecord::Base
 
   attr_accessor :yourevent
 
+  def self.view_event_for_event_planner(role)
+    puts "You have #{role.events.count} events!"
+    parties= Event.where(event_planner_id: role.id)
+    options=parties.map do |party|
+      party.event_name
+    end
+
+    prompt=TTY::Prompt.new
+    move= prompt.select("\n", options)
+
+    options.select do |event|
+      if event == move
+        found_event=Event.find_by(event_name: move)
+        
+        puts "***********************************************************************".colorize(:green) 
+        puts "\n"
+        puts "EVENT NAME: #{found_event.event_name}".colorize(:yellow)
+        puts "DATE: #{found_event.date}".colorize(:yellow)
+        puts "LOCATION: #{found_event.location}".colorize(:yellow)
+        puts "DURATION: #{found_event.duration}".colorize(:yellow)
+        puts "\n"
+          if found_event.event_planner != nil
+        puts "EVENTPLANNER: #{found_event.event_planner.name}".colorize(:red)
+          elsif found_event.event_planner == nil
+        puts "EVENTPLANNER: None".colorize(:red)
+          end
+          if found_event.client != nil
+        puts "CLIENT: #{found_event.client.name}".colorize(:red)
+          elsif found_event.client == nil
+        puts "CLIENT ACCOUNT NUMBER: #{found_event.client.id}".colorize(:red)
+          end
+        puts "\n"
+        puts "***********************************************************************".colorize(:green) 
+      end
+    end
+    puts "\n"
+    puts "....going back to the main page!!".colorize(:yellow)
+    sleep 4
+    YourEvent.home_page(role)
+end
+
     def self.create_event_for_client(role)
         prompt=TTY::Prompt.new
         
